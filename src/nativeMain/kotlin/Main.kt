5a5fb8e77cd6f5082a9e52c8c12740a6bde06dbf
alexander-drabik <xelada91@o2.pl>
2022-01-14 19:01:33 +0000

    Repair ncurses import

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,11 +1,18 @@
+
+import ncurses.endwin
+import ncurses.initscr
+import ncurses.refresh
 import objects.HeaderObject
 
 // Root array of header objects, can be accessed from everywhere via 'import headerObjects'
 var headerObjects: Array<HeaderObject?> =  Array(0) { HeaderObject() }
 
 fun main() {
-    //TODO: printing
-    loadFromFile()
+    initscr()
+
+    refresh()
+
+    endwin()
 }
 
 fun createHeaderObject(headerObject: HeaderObject) {
