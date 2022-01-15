0f5a57526e46275037e7d37765be9dd1c903c835
alexander-drabik <xelada91@o2.pl>
2022-01-15 16:14:57 +0000

    Editing

--- src/nativeMain/kotlin/Keyboard.kt
+++ src/nativeMain/kotlin/Keyboard.kt
@@ -1,20 +1,17 @@
 
-import ncurses.KEY_DOWN
-import ncurses.KEY_LEFT
-import ncurses.KEY_RIGHT
-import ncurses.KEY_UP
+import ncurses.*
 import objects.State
 import kotlin.math.max
 
 class KeyboardEvents {
     fun typeEvent(keycode: Int) {
         when(keycode) {
-            Keyboard.E.keycode -> {
+            Keyboard.Q.keycode -> {
                 close()
             }
             Keyboard.TAB.keycode, Keyboard.ENTER.keycode -> {
                 var line = 0
-                for(headerObject in headerObjects) {
+                loop@ for(headerObject in headerObjects) {
                     if(line == y) {
                         headerObject?.expanded = !headerObject?.expanded!!
                         break
@@ -27,11 +24,28 @@ class KeyboardEvents {
                     }
                 }
             }
+            Keyboard.E.keycode -> {
+                var line = 0
+                loop@ for(headerObject in headerObjects) {
+                    if(line == y){
+                        headerObject!!.headerTitle = editText(headerObject.headerTitle)
+                        saveToFile()
+                        break
+                    }
 
-            KEY_UP   -> y = max(y-1, 0)
-            KEY_DOWN -> y++
-            KEY_LEFT -> x = max(x-1, 0)
-            KEY_RIGHT-> x++
+                    line++
+                    if(headerObject!!.expanded) {
+                        for(todo in headerObject.listOfTODOs) {
+                            if(line == y) {
+                                todo!!.text = editText(todo.text)
+                                saveToFile()
+                                break@loop
+                            }
+                            line++
+                        }
+                    }
+                }
+            }
         }
         when(keycode) {
             Keyboard.ENTER.keycode -> {
@@ -60,10 +74,40 @@ class KeyboardEvents {
                 }
             }
         }
+
+        when(keycode) {
+            KEY_UP   -> y = max(y-1, 0)
+            KEY_DOWN -> y++
+            KEY_LEFT -> x = max(x-1, 0)
+            KEY_RIGHT -> x++
+        }
+    }
+
+    private fun editText(text: String): String {
+        var output = text
+        while (true) {
+            initscr()
+
+            erase()
+            move(0, 0)
+
+            printw(output)
+
+            refresh()
+            val input = getch()
+            if(input == Keyboard.ENTER.keycode || input == Keyboard.TAB.keycode) {
+                return output
+            }
+            if(input == KEY_BACKSPACE) {
+                output = output.dropLast(1)
+            } else {
+                output += input.toChar()
+            }
+        }
     }
 }
 
 // Enum for keycodes that ncurses doesn't offer
 enum class Keyboard(val keycode: Int) {
-    E(101), TAB(9), ENTER(10)
+    E(101), TAB(9), ENTER(10), Q(113), BACKSPACE(127)
 }
